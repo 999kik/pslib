1dfe5554ad45ec16f0c46ecffed67203d0c84ec7
999kik <gggkik@hanyang.ac.kr>
2024-05-27 18:14:42 +0900

    Update seg_max.cpp

--- seg_max.cpp
+++ seg_max.cpp
@@ -3,7 +3,7 @@ struct SEG{
     int n;
     SEG(int x){n=x;t.assign(2*n+1,0);}
     void upd(int p,int v){
-        for(t[p+=n-1]=v;p>0;p>>=1) t[p>>1] = max(t[p],t[p^1]);
+        for(t[p+=n-1]=v;p>1;p>>=1) t[p>>1] = max(t[p],t[p^1]);
     }
     int query(int l,int r){
         int ret = 0;
