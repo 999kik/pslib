6e8c06502008e426f8ce720cdbe116a001e12518
999kik <gggkik@hanyang.ac.kr>
2024-06-13 22:32:10 +0900

    Update bcc.cpp

--- graph/bcc.cpp
+++ graph/bcc.cpp
@@ -1,24 +1,23 @@
 int num[MXN], low[MXN], t;
-vector<vector<int>> bcc;
+vector<int> bcc[MXN];
 void dfs(int x,int p){
     num[x] = low[x] = ++t;
     int lo = t;
-    for(int i : E[x]) if(i^p) {
-        if(!num[x]) dfs(i,x);
+    for(auto i : E[x]) if(i^p) {
+        if(!num[i]) dfs(i,x);
         lo = min(lo,low[i]);
     }
     low[x] = lo;
 }
-int ck[MXN];
+int ck[MXN], bcnt;
 void color(int x,int c){
-    if(c!=-1) bcc[x].push_back(c);
+    if(c) bcc[x].push_back(c);
     ck[x] = 1;
     for(int i : E[x]) if(!ck[i]){
         if (num[x]<=low[i]) {
-			bcc[x].push_back(bcc.size());
-            bcc.push_back({});
-			color(i,bcc.size());
-		}
-		else color(i,c);
+            bcc[x].push_back(++bcnt);
+            color(i,bcnt);
+        }
+        else color(i,c);
     }
 }
