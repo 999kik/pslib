45f03717531ace4bab89c116a49b2941990f87d2
999kik <gggkik@hanyang.ac.kr>
2024-07-07 18:42:23 +0900

    add v2

--- coordinate_compression.cpp
+++ coordinate_compression.cpp
@@ -7,3 +7,15 @@ struct coor{
         for(int &i : A) i = lower_bound(X.begin(),X.end(),i)-X.begin(); 
     };
 };
+/// v2
+struct coor{
+    vector<ll> X;
+    coor(vector<ll> A){
+        X = A;
+        sort(X.begin(),X.end());
+        X.erase(unique(X.begin(),X.end()),X.end());
+    };
+    int operator[](int x){
+        return lower_bound(X.begin(),X.end(),x)-X.begin();
+    }
+};
