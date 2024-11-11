2d25d59289d2133669be5c91aab63cc890f41ed0
999kik <gggkik@hanyang.ac.kr>
2024-11-11 10:48:36 +0900

    Update BIT.cpp

--- BIT.cpp
+++ BIT.cpp
@@ -1,6 +1,6 @@
 struct BIT{
     vector<int> t; int n;
-    BIT(int x){n=x+1;t.assign(n,0);}
+    BIT(int x){n=x+5;t.assign(n,0);}
     void upd(int x,int v){for(x++;x<=n;x+=x&-x)t[x]+=v;}
     int sum(int x){int r=0;for(x++;x;x-=x&-x)r+=t[x];return r;}
     int query(int l,int r){return sum(r)-sum(l-1);}
