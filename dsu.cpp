508feeb49ab058a0af147690a695ee3ce7884750
999kik <gggkik@hanyang.ac.kr>
2025-03-25 09:46:34 +0900

    Update dsu.cpp

--- dsu.cpp
+++ dsu.cpp
@@ -4,3 +4,11 @@ struct dsu{
     int find(int x){return par[x]-x?par[x]=find(par[x]):x;}
     void u(int a,int b){par[find(a)] = find(b);}
 };
+
+// if your happy today
+struct dsu{
+    vector<int> par;
+    dsu(int n){par.resize(n+1); iota(par.begin(),par.end(),0);}
+    int operator[](int x){return par[x]-x?par[x]=(*this)[par[x]]:x;}
+    void u(int a,int b){par[(*this)[a]] = (*this)[b];}
+};
