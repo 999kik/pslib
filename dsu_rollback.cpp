187a531c9179aaa111d25a294e0a94636b4c0b71
999kik <gggkik@hanyang.ac.kr>
2025-06-24 02:23:53 +0900

    Update dsu_rollback.cpp

--- dsu_rollback.cpp
+++ dsu_rollback.cpp
@@ -1,7 +1,7 @@
-struct rollback_UF{
+struct ufrb{
     vector<int> par, sz;
     vector<pii> edges;
-    void init(int x){
+    ufrb(int x){
         par.resize(x+1);
         sz.assign(x+1,1);
         iota(par.begin(),par.end(),0);
@@ -14,16 +14,18 @@ struct rollback_UF{
         a = find(a), b = find(b);
         if(sz[a]<sz[b]) swap(a,b);
         edges.push_back({a,b});
+        if(a==b) return;
         sz[a] += sz[b];
         par[b] = a;
     }
     void rollback(){
         auto [a,b] = edges.back();
+        edges.pop_back();
+        if(a==b) return;
         for(int i = par[b];;i=par[i]){
             sz[i] -= sz[b];
             if(i==par[i]) break;
         }
         par[b] = b;
-        edges.pop_back();
     }
 };
